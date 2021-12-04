8f45386cc7be96251ee0219c57a4817d906f9343
Szymon Woronowicz <szymonworonowicz25@gmail.com>
2021-12-04 13:15:33 +0100

    04.12

--- Lista zadan/Kontrolki/RegisterControl.Designer.cs
+++ Lista zadan/Kontrolki/RegisterControl.Designer.cs
@@ -52,7 +52,7 @@
             this.btnCofnij.BackColor = System.Drawing.Color.PaleTurquoise;
             this.btnCofnij.FlatAppearance.BorderSize = 0;
             this.btnCofnij.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCofnij.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCofnij.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCofnij.Location = new System.Drawing.Point(20, 21);
             this.btnCofnij.Name = "btnCofnij";
             this.btnCofnij.Size = new System.Drawing.Size(98, 68);
@@ -63,50 +63,50 @@
             // lblTitle
             // 
             this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Monotype Corsiva", 40F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitle.Location = new System.Drawing.Point(179, 21);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(245, 65);
+            this.lblTitle.Size = new System.Drawing.Size(310, 63);
             this.lblTitle.TabIndex = 2;
             this.lblTitle.Text = "Rejestracja";
             // 
             // lblLogin
             // 
             this.lblLogin.AutoSize = true;
-            this.lblLogin.Font = new System.Drawing.Font("Monotype Corsiva", 20F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLogin.Location = new System.Drawing.Point(85, 139);
+            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.Location = new System.Drawing.Point(104, 138);
             this.lblLogin.Name = "lblLogin";
-            this.lblLogin.Size = new System.Drawing.Size(79, 33);
+            this.lblLogin.Size = new System.Drawing.Size(79, 29);
             this.lblLogin.TabIndex = 3;
             this.lblLogin.Text = "Login:";
             // 
             // lblHaslo
             // 
             this.lblHaslo.AutoSize = true;
-            this.lblHaslo.Font = new System.Drawing.Font("Monotype Corsiva", 20F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHaslo.Location = new System.Drawing.Point(85, 226);
+            this.lblHaslo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHaslo.Location = new System.Drawing.Point(91, 221);
             this.lblHaslo.Name = "lblHaslo";
-            this.lblHaslo.Size = new System.Drawing.Size(79, 33);
+            this.lblHaslo.Size = new System.Drawing.Size(84, 29);
             this.lblHaslo.TabIndex = 4;
             this.lblHaslo.Text = "Hasło:";
             // 
             // lblPowtorzHaslo
             // 
             this.lblPowtorzHaslo.AutoSize = true;
-            this.lblPowtorzHaslo.Font = new System.Drawing.Font("Monotype Corsiva", 20F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPowtorzHaslo.Location = new System.Drawing.Point(1, 313);
+            this.lblPowtorzHaslo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPowtorzHaslo.Location = new System.Drawing.Point(13, 312);
             this.lblPowtorzHaslo.Name = "lblPowtorzHaslo";
-            this.lblPowtorzHaslo.Size = new System.Drawing.Size(163, 33);
+            this.lblPowtorzHaslo.Size = new System.Drawing.Size(173, 29);
             this.lblPowtorzHaslo.TabIndex = 4;
             this.lblPowtorzHaslo.Text = "Powtórz hasło:";
             // 
             // lblImie
             // 
             this.lblImie.AutoSize = true;
-            this.lblImie.Font = new System.Drawing.Font("Monotype Corsiva", 20F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblImie.Location = new System.Drawing.Point(99, 56);
+            this.lblImie.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImie.Location = new System.Drawing.Point(118, 55);
             this.lblImie.Name = "lblImie";
-            this.lblImie.Size = new System.Drawing.Size(65, 33);
+            this.lblImie.Size = new System.Drawing.Size(65, 29);
             this.lblImie.TabIndex = 4;
             this.lblImie.Text = "Imię:";
             // 
@@ -115,7 +115,7 @@
             this.btnZarejestruj.BackColor = System.Drawing.Color.LightGreen;
             this.btnZarejestruj.FlatAppearance.BorderSize = 0;
             this.btnZarejestruj.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnZarejestruj.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnZarejestruj.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnZarejestruj.Location = new System.Drawing.Point(125, 534);
             this.btnZarejestruj.Name = "btnZarejestruj";
             this.btnZarejestruj.Size = new System.Drawing.Size(246, 86);
@@ -125,47 +125,47 @@
             // 
             // tbLogin
             // 
-            this.tbLogin.Font = new System.Drawing.Font("Monotype Corsiva", 20F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbLogin.Location = new System.Drawing.Point(170, 136);
+            this.tbLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbLogin.Location = new System.Drawing.Point(192, 135);
             this.tbLogin.Name = "tbLogin";
-            this.tbLogin.Size = new System.Drawing.Size(251, 37);
+            this.tbLogin.Size = new System.Drawing.Size(251, 38);
             this.tbLogin.TabIndex = 6;
             // 
             // tbHaslo
             // 
-            this.tbHaslo.Font = new System.Drawing.Font("Monotype Corsiva", 20F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbHaslo.Location = new System.Drawing.Point(170, 222);
+            this.tbHaslo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbHaslo.Location = new System.Drawing.Point(192, 221);
             this.tbHaslo.Name = "tbHaslo";
             this.tbHaslo.PasswordChar = '*';
-            this.tbHaslo.Size = new System.Drawing.Size(251, 37);
+            this.tbHaslo.Size = new System.Drawing.Size(251, 38);
             this.tbHaslo.TabIndex = 7;
             // 
             // tbPowtorzHaslo
             // 
-            this.tbPowtorzHaslo.Font = new System.Drawing.Font("Monotype Corsiva", 20F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbPowtorzHaslo.Location = new System.Drawing.Point(170, 307);
+            this.tbPowtorzHaslo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPowtorzHaslo.Location = new System.Drawing.Point(192, 306);
             this.tbPowtorzHaslo.Name = "tbPowtorzHaslo";
             this.tbPowtorzHaslo.PasswordChar = '*';
-            this.tbPowtorzHaslo.Size = new System.Drawing.Size(251, 37);
+            this.tbPowtorzHaslo.Size = new System.Drawing.Size(251, 38);
             this.tbPowtorzHaslo.TabIndex = 7;
             // 
             // tbImie
             // 
-            this.tbImie.Font = new System.Drawing.Font("Monotype Corsiva", 20F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbImie.Location = new System.Drawing.Point(170, 56);
+            this.tbImie.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbImie.Location = new System.Drawing.Point(192, 56);
             this.tbImie.Name = "tbImie";
-            this.tbImie.Size = new System.Drawing.Size(251, 37);
+            this.tbImie.Size = new System.Drawing.Size(251, 38);
             this.tbImie.TabIndex = 7;
             // 
             // lblNameValidation
             // 
             this.lblNameValidation.AutoEllipsis = true;
             this.lblNameValidation.AutoSize = true;
-            this.lblNameValidation.Font = new System.Drawing.Font("Monotype Corsiva", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNameValidation.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblNameValidation.ForeColor = System.Drawing.Color.Red;
-            this.lblNameValidation.Location = new System.Drawing.Point(166, 96);
+            this.lblNameValidation.Location = new System.Drawing.Point(199, 97);
             this.lblNameValidation.Name = "lblNameValidation";
-            this.lblNameValidation.Size = new System.Drawing.Size(160, 24);
+            this.lblNameValidation.Size = new System.Drawing.Size(193, 25);
             this.lblNameValidation.TabIndex = 8;
             this.lblNameValidation.Text = "Labelka na walidacje";
             this.lblNameValidation.Visible = false;
@@ -174,11 +174,11 @@
             // 
             this.lblRepeatPasswordValidation.AutoEllipsis = true;
             this.lblRepeatPasswordValidation.AutoSize = true;
-            this.lblRepeatPasswordValidation.Font = new System.Drawing.Font("Monotype Corsiva", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRepeatPasswordValidation.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblRepeatPasswordValidation.ForeColor = System.Drawing.Color.Red;
-            this.lblRepeatPasswordValidation.Location = new System.Drawing.Point(166, 347);
+            this.lblRepeatPasswordValidation.Location = new System.Drawing.Point(199, 347);
             this.lblRepeatPasswordValidation.Name = "lblRepeatPasswordValidation";
-            this.lblRepeatPasswordValidation.Size = new System.Drawing.Size(160, 24);
+            this.lblRepeatPasswordValidation.Size = new System.Drawing.Size(193, 25);
             this.lblRepeatPasswordValidation.TabIndex = 9;
             this.lblRepeatPasswordValidation.Text = "Labelka na walidacje";
             this.lblRepeatPasswordValidation.Visible = false;
@@ -187,11 +187,11 @@
             // 
             this.lblLoginValidation.AutoEllipsis = true;
             this.lblLoginValidation.AutoSize = true;
-            this.lblLoginValidation.Font = new System.Drawing.Font("Monotype Corsiva", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoginValidation.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblLoginValidation.ForeColor = System.Drawing.Color.Red;
-            this.lblLoginValidation.Location = new System.Drawing.Point(166, 176);
+            this.lblLoginValidation.Location = new System.Drawing.Point(199, 176);
             this.lblLoginValidation.Name = "lblLoginValidation";
-            this.lblLoginValidation.Size = new System.Drawing.Size(160, 24);
+            this.lblLoginValidation.Size = new System.Drawing.Size(193, 25);
             this.lblLoginValidation.TabIndex = 9;
             this.lblLoginValidation.Text = "Labelka na walidacje";
             this.lblLoginValidation.Visible = false;
@@ -200,11 +200,11 @@
             // 
             this.lblPasswordValidation.AutoEllipsis = true;
             this.lblPasswordValidation.AutoSize = true;
-            this.lblPasswordValidation.Font = new System.Drawing.Font("Monotype Corsiva", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPasswordValidation.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPasswordValidation.ForeColor = System.Drawing.Color.Red;
-            this.lblPasswordValidation.Location = new System.Drawing.Point(166, 262);
+            this.lblPasswordValidation.Location = new System.Drawing.Point(199, 262);
             this.lblPasswordValidation.Name = "lblPasswordValidation";
-            this.lblPasswordValidation.Size = new System.Drawing.Size(160, 24);
+            this.lblPasswordValidation.Size = new System.Drawing.Size(193, 25);
             this.lblPasswordValidation.TabIndex = 9;
             this.lblPasswordValidation.Text = "Labelka na walidacje";
             this.lblPasswordValidation.Visible = false;
@@ -223,7 +223,7 @@
             this.groupBoxUserData.Controls.Add(this.tbImie);
             this.groupBoxUserData.Controls.Add(this.tbLogin);
             this.groupBoxUserData.Controls.Add(this.tbHaslo);
-            this.groupBoxUserData.Font = new System.Drawing.Font("Monotype Corsiva", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxUserData.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBoxUserData.Location = new System.Drawing.Point(20, 107);
             this.groupBoxUserData.Name = "groupBoxUserData";
             this.groupBoxUserData.Size = new System.Drawing.Size(458, 412);
--- Lista zadan/Kontrolki/TasksControl.Designer.cs
+++ Lista zadan/Kontrolki/TasksControl.Designer.cs
@@ -28,18 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnWyloguj = new System.Windows.Forms.Button();
+            this.lblZalogowny = new System.Windows.Forms.Label();
+            this.lblZalogowanyWartosc = new System.Windows.Forms.Label();
+            this.lblKreski = new System.Windows.Forms.Label();
+            this.lvZadania = new System.Windows.Forms.ListView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.lblTodoTitle = new System.Windows.Forms.Label();
+            this.columnId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnTytul = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnZrobione = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
+            // 
+            // btnWyloguj
+            // 
+            this.btnWyloguj.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btnWyloguj.FlatAppearance.BorderSize = 0;
+            this.btnWyloguj.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWyloguj.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWyloguj.Location = new System.Drawing.Point(424, 11);
+            this.btnWyloguj.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnWyloguj.Name = "btnWyloguj";
+            this.btnWyloguj.Size = new System.Drawing.Size(68, 56);
+            this.btnWyloguj.TabIndex = 1;
+            this.btnWyloguj.Text = "🏃";
+            this.btnWyloguj.UseVisualStyleBackColor = false;
+            // 
+            // lblZalogowny
+            // 
+            this.lblZalogowny.AutoSize = true;
+            this.lblZalogowny.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblZalogowny.Location = new System.Drawing.Point(133, 28);
+            this.lblZalogowny.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblZalogowny.Name = "lblZalogowny";
+            this.lblZalogowny.Size = new System.Drawing.Size(165, 25);
+            this.lblZalogowny.TabIndex = 2;
+            this.lblZalogowny.Text = "Zalogowany jako:";
+            // 
+            // lblZalogowanyWartosc
+            // 
+            this.lblZalogowanyWartosc.AutoSize = true;
+            this.lblZalogowanyWartosc.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblZalogowanyWartosc.Location = new System.Drawing.Point(266, 27);
+            this.lblZalogowanyWartosc.Name = "lblZalogowanyWartosc";
+            this.lblZalogowanyWartosc.Size = new System.Drawing.Size(100, 29);
+            this.lblZalogowanyWartosc.TabIndex = 3;
+            this.lblZalogowanyWartosc.Text = "Anonim";
+            // 
+            // lblKreski
+            // 
+            this.lblKreski.AutoSize = true;
+            this.lblKreski.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKreski.Location = new System.Drawing.Point(3, 68);
+            this.lblKreski.Name = "lblKreski";
+            this.lblKreski.Size = new System.Drawing.Size(495, 9);
+            this.lblKreski.TabIndex = 5;
+            this.lblKreski.Text = "_________________________________________________________________________________" +
+    "_________________";
+            // 
+            // lvZadania
+            // 
+            this.lvZadania.BackColor = System.Drawing.SystemColors.Info;
+            this.lvZadania.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnId,
+            this.columnTytul,
+            this.columnZrobione});
+            this.lvZadania.FullRowSelect = true;
+            this.lvZadania.GridLines = true;
+            this.lvZadania.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lvZadania.HideSelection = false;
+            this.lvZadania.Location = new System.Drawing.Point(5, 87);
+            this.lvZadania.MultiSelect = false;
+            this.lvZadania.Name = "lvZadania";
+            this.lvZadania.ShowGroups = false;
+            this.lvZadania.Size = new System.Drawing.Size(413, 548);
+            this.lvZadania.TabIndex = 6;
+            this.lvZadania.UseCompatibleStateImageBehavior = false;
+            this.lvZadania.View = System.Windows.Forms.View.Details;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.LightGreen;
+            this.btnAdd.FlatAppearance.BorderSize = 0;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(424, 87);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(68, 210);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "➕";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Tomato;
+            this.btnDelete.FlatAppearance.BorderSize = 0;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft JhengHei UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(424, 529);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(68, 106);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "🗑";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.Turquoise;
+            this.btnEdit.FlatAppearance.BorderSize = 0;
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft JhengHei UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(424, 303);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(68, 220);
+            this.btnEdit.TabIndex = 10;
+            this.btnEdit.Text = "🖉";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            // 
+            // lblTodoTitle
+            // 
+            this.lblTodoTitle.AutoSize = true;
+            this.lblTodoTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTodoTitle.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.lblTodoTitle.Location = new System.Drawing.Point(3, 11);
+            this.lblTodoTitle.Name = "lblTodoTitle";
+            this.lblTodoTitle.Size = new System.Drawing.Size(139, 46);
+            this.lblTodoTitle.TabIndex = 11;
+            this.lblTodoTitle.Text = "TODO";
+            // 
+            // columnId
+            // 
+            this.columnId.Text = "Id";
+            this.columnId.Width = 35;
+            // 
+            // columnTytul
+            // 
+            this.columnTytul.Text = "Tytul";
+            this.columnTytul.Width = 281;
+            // 
+            // columnZrobione
+            // 
+            this.columnZrobione.Text = "Zrobione";
+            this.columnZrobione.Width = 287;
             // 
             // TasksControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.Controls.Add(this.lblTodoTitle);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lvZadania);
+            this.Controls.Add(this.lblKreski);
+            this.Controls.Add(this.lblZalogowanyWartosc);
+            this.Controls.Add(this.lblZalogowny);
+            this.Controls.Add(this.btnWyloguj);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.Name = "TasksControl";
             this.Size = new System.Drawing.Size(500, 640);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Button btnWyloguj;
+        private System.Windows.Forms.Label lblZalogowny;
+        private System.Windows.Forms.Label lblZalogowanyWartosc;
+        private System.Windows.Forms.Label lblKreski;
+        private System.Windows.Forms.ListView lvZadania;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Label lblTodoTitle;
+        private System.Windows.Forms.ColumnHeader columnId;
+        private System.Windows.Forms.ColumnHeader columnTytul;
+        private System.Windows.Forms.ColumnHeader columnZrobione;
     }
 }
